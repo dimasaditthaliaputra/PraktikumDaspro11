2709b39074b16700bd1575b50ce4ebf6c1a9122a
dimasaditt <aditcraftyt@gmail.com>
2024-11-15 13:21:42 +0700

    🌏Edit Tugas 2

--- Tugas206.java
+++ Tugas206.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Tugas206 {
-    static int[][] nilaiMahasiswa;
-    static String[] namaMahasiswa;
+    static int[][] nilaiMahasiswa = new int[5][7];
+    static String[] namaMahasiswa = { "Sari", "Rina", "Yani", "Dwi", "Lusi" };
 
     public static void inputData() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
-            System.out.print("Masukkan nama mahasiswa ke-" + (i + 1) + ": ");
-            namaMahasiswa[i] = scanner.next();
-            System.out.println("Masukkan nilai untuk " + namaMahasiswa[i] + " (tugas 1 hingga " + nilaiMahasiswa[i].length + "):");
+            System.out.println("Masukkan nilai untuk " + namaMahasiswa[i] + " (minggu 1 hingga 7):");
             for (int j = 0; j < nilaiMahasiswa[i].length; j++) {
-                System.out.print("Tugas ke-" + (j + 1) + ": ");
+                System.out.print("Minggu ke-" + (j + 1) + ": ");
                 nilaiMahasiswa[i][j] = scanner.nextInt();
             }
             System.out.println();
@@ -20,11 +18,7 @@ public class Tugas206 {
 
     public static void tampilkanNilai() {
         System.out.println("Nilai Mahasiswa:");
-        System.out.print("Nama\t");
-        for (int i = 0; i < nilaiMahasiswa[0].length; i++) {
-            System.out.print("T" + (i + 1) + "\t");
-        }
-        System.out.println();
+        System.out.println("Nama\tM1\tM2\tM3\tM4\tM5\tM6\tM7");
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
             System.out.print(namaMahasiswa[i] + "\t");
             for (int j = 0; j < nilaiMahasiswa[i].length; j++) {
@@ -36,49 +30,41 @@ public class Tugas206 {
 
     public static void cariMingguNilaiTertinggi() {
         int maxNilai = 0;
-        int tugasTertinggi = 0;
+        int mingguTertinggi = 0;
 
-        for (int j = 0; j < nilaiMahasiswa[0].length; j++) {
-            int totalTugas = 0;
-            for (int i = 0; i < nilaiMahasiswa.length; i++) {
-                totalTugas += nilaiMahasiswa[i][j];
+        for (int j = 0; j < 7; j++) {
+            int totalMinggu = 0;
+            for (int i = 0; i < 5; i++) {
+                totalMinggu += nilaiMahasiswa[i][j];
             }
-            if (totalTugas > maxNilai) {
-                maxNilai = totalTugas;
-                tugasTertinggi = j + 1;
+            if (totalMinggu > maxNilai) {
+                maxNilai = totalMinggu;
+                mingguTertinggi = j + 1;
             }
         }
-        System.out.println("Tugas dengan nilai tertinggi adalah Tugas ke-" + tugasTertinggi + " dengan total nilai " + maxNilai);
+        System.out.println("Minggu dengan nilai tertinggi adalah Minggu ke-" + mingguTertinggi + " dengan total nilai "
+                + maxNilai);
     }
 
     public static void tampilkanMahasiswaNilaiTertinggi() {
         int maxNilai = 0;
         String nama = "";
-        int tugas = 0;
+        int minggu = 0;
 
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
             for (int j = 0; j < nilaiMahasiswa[i].length; j++) {
                 if (nilaiMahasiswa[i][j] > maxNilai) {
                     maxNilai = nilaiMahasiswa[i][j];
                     nama = namaMahasiswa[i];
-                    tugas = j + 1;
+                    minggu = j + 1;
                 }
             }
         }
-        System.out.println("Mahasiswa dengan nilai tertinggi adalah " + nama + " dengan nilai " + maxNilai + " pada Tugas ke-" + tugas);
+        System.out.println("Mahasiswa dengan nilai tertinggi adalah " + nama + " dengan nilai " + maxNilai
+                + " pada Minggu ke-" + minggu);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlahMahasiswa = scanner.nextInt();
-        System.out.print("Masukkan jumlah tugas: ");
-        int jumlahTugas = scanner.nextInt();
-
-        // Inisialisasi array sesuai input user
-        nilaiMahasiswa = new int[jumlahMahasiswa][jumlahTugas];
-        namaMahasiswa = new String[jumlahMahasiswa];
-
         inputData();
         tampilkanNilai();
         cariMingguNilaiTertinggi();
